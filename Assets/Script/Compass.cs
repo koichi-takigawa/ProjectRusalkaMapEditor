7ff2c86b358e26482b360de78e1d0d3bc1e6dd53
Koichi Takigawa <tuk_@hotmail.co.jp>
2026-05-10 23:11:46 +0900

    コンパス表示の追加 com.unity.2d.spriteの追加

---
+++ Assets/Script/Compass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class Compass : MonoBehaviour
+{
+    // コンパスの針（または盤面）のRectTransform
+    private RectTransform mCompassRect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.TryGetComponentWithError<RectTransform>(out mCompassRect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraTransform = CameraController.Instance.MainCamera.transform;
+        mCompassRect.localRotation = Quaternion.Euler(90 - cameraTransform.eulerAngles.x, 0, cameraTransform.eulerAngles.y);
+    }
+}
